e1b69cffca575285ff308924831f7fa2032eb372
Olivier Broekman <olivier.broekman@ru.nl>
2023-12-07 14:52:24 +0100

    modified:   test.json     Changed some stuff to isolate testing 	modified:   wip/test.py     Started on findDurability() function.

--- wip/test.py
+++ wip/test.py
@@ -1,19 +1,6 @@
 import json
 import pprint as pprint
 
-# def findID(filePath):
-#     name = input("Enter item name: ").lower()
-#     with open(filePath, 'r', encoding='utf-8') as f:
-#         data = json.load(f)
-        
-#     for id, info in data.items():
-#         if info['name'].lower() == name:
-#             return id
-#         if info['shortname'] == name:
-#             return id
-
-#     return "Not a valid item name."
-
 def findID(filePath, search_by_id=False):
     search_term = input("Enter item name or id: ").lower()
     with open(filePath, 'r', encoding='utf-8') as f:
@@ -38,19 +25,13 @@ def findDurability(filePath, searchType=None):
     id = findID(itemsFile)
     with open(filePath, 'r', encoding='utf-8') as f:
         data = json.load(f)
-    return data[searchType][id]['durability']
+    for info in data.values():
+        for item, dictionary in info.items():
+            if item == id:
+                return 'Item Found'
     
-# def findName(filePath, searchID):
-#     with open(filePath, 'r', encoding='utf-8') as f:
-#         data = json.load(f)
-        
-#     for id, info in data.items():
-#         if id == searchID:
-#             return info['name']
 
-#     return "Not a valid ID."
+itemsFile = r'items.json'
+durabFile = r'rustlabsDurabilityData.json'
 
-itemsFile = r'Python\Rust-Raid-Calculator\Test\items.json'
-durabFile = r'Python\Rust-Raid-Calculator\Test\rustlabsDurabilityData.json'
-
-print(findID(itemsFile))
+print(findDurability(durabFile))
