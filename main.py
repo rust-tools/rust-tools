b0aab4a8d56c6152511a75844e1d1f67eeb506ad
Bernd van Ruremonde <berndvanruremonde@gmail.com>
2023-12-21 00:16:49 +0100

    clarified that the file is a runnable script

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 # Import functions.py as cost
 import functions as cost
 
-# File Locations
-itemsFile = r'data\items.json'
-durabFile = r'data\rustlabsDurabilityData.json'
+def consoleCalc():
+    # File Locations
+    itemsFile = r'data\items.json'
+    durabFile = r'data\rustlabsDurabilityData.json'
 
-# Inputs
-input1 = input("What do you want to raid? ")
-input2 = input("What type of item is this? (B,V,D)? ")
-input3 = input("What type of raid do you want to do? (eco/explo) ")
+    # Inputs
+    input1 = input("What do you want to raid? ")
+    input2 = input("What type of item is this? (B,V,D)? ")
+    input3 = input("What type of raid do you want to do? (eco/explo) ")
 
 
-# Print the output of the function (pretty self explanatory)
-if input2 == 'B':
-    print(cost.findDurability('building', input1, input3))
-elif input2 == 'V':
-    print(cost.findDurability('vehicle', input1, input3))
-elif input2 == 'D':
-    print(cost.findDurability('deployable', input1, input3))
+    # Print the output of the function (pretty self explanatory)
+    if input2 == 'B':
+        print(cost.findDurability('building', input1, input3))
+    elif input2 == 'V':
+        print(cost.findDurability('vehicle', input1, input3))
+    elif input2 == 'D':
+        print(cost.findDurability('deployable', input1, input3))
+        
+if __name__ == "__main__":
+    consoleCalc()
