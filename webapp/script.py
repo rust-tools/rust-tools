915649927faed145aa8c5fc51e9f40cc49b964c2
Bernd van Ruremonde <berndvanruremonde@gmail.com>
2023-12-21 01:35:15 +0100

    commented the file

--- webapp/script.py
+++ webapp/script.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request
 import pythonFunctions
 
+# Declares the app variable to start the webapp
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+# Renders the index.html page with the result variable. If the request method is POST, it will run the pythonFunctions.findDurability function and return the result to the index.html page
+@app.route('/', methods=['GET', 'POST']) 
 def home():
     if request.method == 'POST':
         input_data = request.form.get('getId')
@@ -14,5 +16,6 @@ def home():
         return render_template('index.html', result=result)
     return render_template('index.html', result="waiting for input")
 
+# Runs the app
 if __name__ == "__main__":
     app.run(debug=True)
