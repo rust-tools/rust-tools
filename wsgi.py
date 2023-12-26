54e11e22176192874a236ac41c7c1c5ca405eb3c
Bernd van Ruremonde <berndvanruremonde@gmail.com>
2023-12-26 01:58:39 +0100

    modified:   heroku.yml 	modified:   wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,11 @@
 import sys
+import os
 path = 'webapp/app.py'
 if path not in sys.path:
    sys.path.insert(0, path)
 
-from app import app as application
+from app import app
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
