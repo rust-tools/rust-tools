06398351377a569ed0a7cb928e79725a8b77d65a
Bernd van Ruremonde <berndvanruremonde@gmail.com>
2023-12-27 20:16:28 +0100

    added appSettings.py dependancies

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template, request
 import pythonFunctions
 import os
+from appSettings import useLegacy, debugMode
+
+# Declare proper settings
+if useLegacy:
+    print("Using legacy UI")
+    use_template = "index_legacy.html"
+elif useLegacy == False:
+    print("Using new UI")
+    use_template = "index.html"
 
 # Declares the app variable to start the webapp
 app = Flask(__name__)
@@ -14,8 +23,8 @@ def home():
         itemType = request.form.get('itemType')
         print(itemType, input_data, raidType)
         result = pythonFunctions.findDurability(itemType, input_data, raidType)
-        return render_template('index.html', result=result)
-    return render_template('index.html', result="waiting for input")
+        return render_template(use_template, result=result)
+    return render_template(use_template, result="waiting for input")
 
 # Runs the app
 if __name__ == "__main__":
