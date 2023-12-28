dc287a321759cacfba47912b2db69680dfe2f056
Bernd van Ruremonde <berndvanruremonde@gmail.com>
2023-12-28 03:49:48 +0100

    added page not found render

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ from appSettings import useLegacy, debugMode
 
 # Declare proper settings
 if useLegacy:
-    print("Using legacy UI")
+    print("[CONSOLE] Using legacy UI")
     use_template = "index_legacy.html"
 elif useLegacy == False:
-    print("Using new UI")
+    print("[CONSOLE] Using new UI")
     use_template = "index.html"
 
 # Declares the app variable to start the webapp
@@ -16,15 +16,15 @@ app = Flask(__name__)
 
 # Renders the index.html page with the result variable. If the request method is POST, it will run the pythonFunctions.findDurability function and return the result to the index.html page
 @app.route('/raidtool', methods=['GET', 'POST']) 
-def home():
+def raidTool():
     if request.method == 'POST':
         input_data = request.form.get('getId')
         raidType = request.form.get('raidType')
         itemType = request.form.get('itemType')
-        print(itemType, input_data, raidType)
+        print(f"[CONSOLE] Itemtype: {itemType}, input: {input_data}, raidtype: {raidType}")
         result = pythonFunctions.findDurability(itemType, input_data, raidType)
         return render_template('raidtool.html', result=result)
-    return render_template('raidtool.html', result="waiting for input")
+    return render_template('raidtool.html', result="Waiting for input...")
 
 @app.route('/')
 def index():
@@ -34,7 +34,11 @@ def index():
 def homePage():
     return render_template(use_template)
 
+@app.route('/404')
+def errorPage():
+    return render_template('pageNotFound.html')
+
 # Runs the app
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=debugMode)
