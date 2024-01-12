ae960fdbaeaf1d04b63e1a1b71af8da048fa3de0
Olivier Broekman <olivier.broekman@ru.nl>
2024-01-12 11:14:36 +0100

    deleted:    scraper.py

--- scraper.py
+++
@@ -1,5 +0,0 @@
-import json
-
-lst = ['MLRS Rocket', '40mm HE Grenade', 'Rocket', 'High Velocity Rocket', 'Homing Missile', 'Torpedo', 'Timed Explosive Charge', 'Beancan Grenade', 'F1 Grenade', 'Satchel Charge', 'Explosive 5.56 Rifle Ammo']
-
-print(len(lst))
